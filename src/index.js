1680182b3dd7b075be35b8019343b29ad8fba836
ir-afn <46682788+ir-afn@users.noreply.github.com>
2021-03-11 20:47:45 +0300

    feat: solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (arr) {
+  if (arr === undefined || arr.length === 0) {
+    return 0;
+  }
+  let [theMin, ...rest] = arr;
+  for (const item of rest) {
+    if (item < theMin) {
+      theMin = item;
+    }
+  }
+  return theMin;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (arr) {
+  if (arr === undefined || arr.length === 0) {
+    return 0;
+  }
+  let [theMax, ...rest] = arr;
+  for (const item of rest) {
+    if (item > theMax) {
+      theMax = item;
+    }
+  }
+  return theMax;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (arr) {
+  if (arr === undefined || arr.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (const item of arr) {
+    sum += item;
+  }
+  return sum / arr.length;
+};
